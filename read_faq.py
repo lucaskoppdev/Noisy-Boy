f031ad25f786d2dd3e71f460553139520369bcb2
Lucas Renan <lucaskoppfranca@gmail.com>
2026-01-27 08:52:31 +0000

    vetorizador refinado, com stopwords em portugues e regex para nao escapar palavras

--- read_faq.py
+++ read_faq.py
@@ -2,13 +2,15 @@
 import pandas as pd
 
 
+#Possivelmente esses parametros sera passados como parametros fixos
+    
 
-df = pd.read_excel('perguntas_faq.xlsx', sheet_name = 'Planilha1')
+
+df = pd.read_excel('faq.xlsx', sheet_name = 'Planilha1')
 
 
 #Aqui ja retorna o Json como uma lista de dicionarios
-df.to_json('faq_json.json', indent = 4, orient = 'records', force_ascii = True)
+df_output = df.to_json('faq_json.xlsx', indent = 4, orient = 'records', force_ascii = True)
 
 
 print("Json salvo com sucesso")
-
--- tf-idf_vectorizer.py
+++ tf-idf_vectorizer.py
@@ -1,22 +1,58 @@
 import json
+from pathlib import Path
 from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+
+
+
+def check_if_stopwords_exist():
+    stopwords_confirm = Path("stopwords_exist.txt")
+
+    if stopwords_confirm.is_file():
+        return True
+    
+    else:
+        nltk.download("stopwords")
+
+        with open("stopwords_exist.txt", "x") as file:
+            file.write("melhorando o runtime e a logica do codigo, esse arquivo so serve pra passar pela logica de instalar as stopwords toda vez que o script do TF-IDF RODAR =)")
+            return False
+
+
 
 
 with open('faq_json.json', 'r', encoding = 'utf-8') as corpus:
     file = json.load(corpus)
 
 
+
 #list compaharasion para passar texto para a funcao do tf-idf
 json_to_text = [item["Resposta"] for item in file]
 
 
-vectorizer = TfidfVectorizer()
+
+check_if_stopwords_exist()
+from nltk.corpus import stopwords
+
+portuguese_stopwords = stopwords.words("portuguese")
+
+
+vectorizer = TfidfVectorizer(
+    stop_words = portuguese_stopwords,
+    lowercase = True,
+    token_pattern = r'(?u)\b[a-záàâãéêíóôõúüç]+\b',
+    min_df = 1,
+    max_df = 0.9
+)
+
 
 
 X = vectorizer.fit_transform(json_to_text)
 
 
+
 feature_names = vectorizer.get_feature_names_out()
+
 
 
 print(feature_names)
